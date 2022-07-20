08f05a6dd435832441f2276b16037ba3d103f31c
lyuhan2828 <98847835+lyuhan2828@users.noreply.github.com>
2022-07-20 14:41:28 -0400

    update post

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
-const postRoutes = require('.post-routes');
+const postRoutes = require('./post-routes');
 
 router.use('/users', userRoutes);
-router.use('/post', postRoute)
+router.use('/post', postRoutes)
 
 module.exports = router;
