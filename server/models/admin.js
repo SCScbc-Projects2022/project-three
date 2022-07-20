29aa38e9413a9da09a6c05251bb4aa08552d4e5a
TOVTC <veronica.tc.to@outlook.com>
2022-07-20 15:37:56 -0400

    updated Admin model

--- server/models/admin.js
+++ server/models/admin.js
@@ -13,20 +13,23 @@ const adminSchema = new Schema(
             type: String,
             unique: true,
             required: true,
-            match: [
-                /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                'Please add a valid email address.',
-            ]
+            match: [/.+@.+\..+/, 'Must use a valid email address']
         },
         password: {
             type: String,
             required: true,
             minlength: 6
         },
-        // add management model arrays here
+        // add management arrays here
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
     }
 )
 
+// password hashing
 adminSchema.pre('save', async function(next) {
     if (this.isNew || this.isModified('password')) {
         this.password = await bcrypt.hash(this.password, 10);
--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,4 @@
+const Admin = require('./Admin')
 const User = require('./User');
 
-module.exports = { User };
+module.exports = { Admin, User };
