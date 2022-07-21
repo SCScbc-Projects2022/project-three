53b9131c987ad0385b08e3e32ce398d067c7798e
Csabbah <csabbah@ryerson.ca>
2022-07-20 21:13:22 -0400

    Updated Location model

--- server/models/Location.js
+++ server/models/Location.js
@@ -1,40 +1,18 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = require('./User');
+const Company = require('./Company');
 
-const locationSchema = new Schema(
-    {
-        // meant to hold a store or chain or location number
-        location: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true
-        },
-        street: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true
-        },
-        city: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        country: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        phone: {
-            type: Number,
-            required: true,
-            trim: true
-        },
-        employees: [userSchema]
-    }
-);
+const locationSchema = new Schema({
+  address: {
+    type: Object,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  storeId: Company,
+  employees: [userSchema],
+});
 
 const Location = model('Location', locationSchema);
 
