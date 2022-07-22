00e43ddd03db6b2dfc1128e63a6cb422582f6381
Candice Hall <hall.candice@outlook.com>
2022-07-22 16:04:33 -0400

    starting queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -11,8 +11,53 @@ export const GET_ME = gql`
 `;
 
 // Query Company
+
+export const GET_COMPANY = gql`
+{
+  query companies($name: String) {
+    company(name: $name){
+      name
+      username
+      email
+      postsArr {
+        _id
+        shiftTime
+        additionalInfo
+        locationArr {
+          _id
+          address
+          storeId
+          employees {
+           _id
+            firstName
+            lastName
+            username
+            role
+            email
+            phone
+            location
+          }
+        },
+        role
+        tags {
+          _id
+          title
+        }
+      }
+    }
+  }
+}`
 // Query Location
+
+export const GET_LOCATION = gql`
+{
+query locations()
+}`
 // Query Post
+export const GET_POSTS = gql`
+{
+query posts($id)
+}`
 
 // Query Role
 // Query Tag
