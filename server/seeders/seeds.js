e9b4110b92d605e03c812385503dd587e0b7c141
lyuhan2828 <98847835+lyuhan2828@users.noreply.github.com>
2022-07-23 12:25:17 -0400

    seeds are running now

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -5,10 +5,8 @@ const PostData = require('./post-seeds.json');
 const LocationData = require('./location-seeds.json');
 const CompanyData = require('./company-seeds.json');
 // const mongoose = require('mongoose');
-
 const {Company, Location, Post, Role, Tag, User} = require('../models');
 const db = require('../config/connection');
-
 db.once('open', async() => {
     // clean database
     await Post.deleteMany({});
@@ -17,16 +15,13 @@ db.once('open', async() => {
     await Company.deleteMany({});
     await Role.deleteMany({});
     await Tag.deleteMany({});
-
 //bulk create each model
 const tags = await Tag.insertMany(TagData);
 const role = await Role.insertMany(RoleData);
 const users = await User.insertMany(UserData);
 const companies = await Company.insertMany(CompanyData);
 const locations = await Location.insertMany(LocationData);
-
 // add to each location the company id and users
-
 const locationsStores = locations.map(async (location) => {
     const company = companies.find(
       (company) => company.name == location.address.storeName
@@ -39,7 +34,6 @@ const locationsStores = locations.map(async (location) => {
     const locationUsersIds = locationUsers.map((user) => user._id);
     // console.log(company.name,company._id,location._id)
     // console.log(locationUsers)
-​
     const updateStore = await Location.updateOne(
       { _id: location._id },
       { $push: { storeId: [company._id], employees: locationUsersIds } },
@@ -48,7 +42,6 @@ const locationsStores = locations.map(async (location) => {
     return updateStore;
   });
   const insertLocations = await Promise.all(locationsStores);
-​
   // console.log(insertLocations)
   const posts = await Post.insertMany([
     ...PostData.map((post) => ({
@@ -58,7 +51,6 @@ const locationsStores = locations.map(async (location) => {
   ]);
   //add locations to this post
   // console.log(posts)
-​
   // add the postArr and locationsArr to the companies we created
   const companyArrays = companies.map(async (company) => {
     const locations = await Location.find({
@@ -67,10 +59,8 @@ const locationsStores = locations.map(async (location) => {
     const locationsIds = locations.map((location) => location._id);
     const posts = await Post.find({ locationArr: { $in: locationsIds } });
     const postsIds = posts.map((post) => post._id);
-​
     // console.log(locationsIds);
     // console.log(postsIds);
-​
     return (updateStore = await Company.updateOne(
       { _id: company._id },
       { $push: { postsArr: postsIds, locationArr: locationsIds } },
@@ -79,7 +69,6 @@ const locationsStores = locations.map(async (location) => {
   });
   const insertCompanyArrays = await Promise.all(companyArrays);
   console.log(insertCompanyArrays);
-​
   console.log("testing");
   process.exit(0);
 });
