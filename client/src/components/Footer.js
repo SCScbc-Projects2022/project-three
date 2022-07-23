4b209e6c75717fbeab916080d4c3187b51d03733
Cory Sillaots <csillaots@gmail.com>
2022-07-22 23:02:50 -0400

    updated footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,6 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
 (<div
 style="background-color: dodgerblue; color: white"
 class="text-center sticky-bottom mt-5"
 >
 <div class="card card-footer">Made with ♥ by the Dinglebops</div>
 </div>)
+
+        </>
+    )
+}
+
+export default Footer;
