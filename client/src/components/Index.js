faa1023734a6f7dd4b415f0a1581c9b70761fb5f
Cory Sillaots <csillaots@gmail.com>
2022-07-22 22:18:13 -0400

    merging to react

--- client/src/components/Index.js
+++ client/src/components/Index.js
@@ -3,7 +3,51 @@ import React from 'react';
 const Index = () => {
     return (
         <>
-
+    <div>
+      <img src="../assets/img/hero.jpg" width="100%" height="auto" alt="" />
+    </div>
+     <div
+      class="d-flex flex-column justify-content-center align-items-center ms-5 me-5"
+    >
+      <div class="mt-5">
+        <h1 class="fw-bold">Never be short-staffed again</h1>
+      </div>
+      <div class="text-center">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae
+          velit aliquam a, earum excepturi cupiditate reiciendis atque esse in
+          ipsum similique delectus fugit, debitis cum maiores! Asperiores, quod
+          numquam. Porro consequatur nemo ex architecto dolores. Excepturi,
+          blanditiis accusamus. Cum, architecto illo? Soluta blanditiis dolor
+          quisquam iste. Facere unde dolorum itaque?
+        </p>
+      </div>
+      <div class="container-fluid mt-5">
+        <img src="../assets/img/server.jpg" width="100%" height="auto" alt="" />
+      </div>
+      <div class="mt-5">
+        <h1 class="fw-bold text-center">
+          Expand your available workforce instantly
+        </h1>
+      </div>
+      <div class="text-center">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae
+          velit aliquam a, earum excepturi cupiditate reiciendis atque esse in
+          ipsum similique delectus fugit, debitis cum maiores! Asperiores, quod
+          numquam. Porro consequatur nemo ex architecto dolores. Excepturi,
+          blanditiis accusamus. Cum, architecto illo? Soluta blanditiis dolor
+          quisquam iste. Facere unde dolorum itaque?
+        </p>
+      </div>
+      <div class="mb-5 mt-4">
+        <a href="./sign-up.html"
+          ><button type="button" class="btn btn-primary btn-lg">
+            Sign Up
+          </button></a
+        >
+      </div>
+    </div>
         </>
     )
 }
