806d8e4546f1d337d678b891f95a1b8ec89be3b9
Cory Sillaots <csillaots@gmail.com>
2022-07-22 22:24:59 -0400

    merging to react

---
+++ client/src/components/CompanyProfile.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const CompanyProfile = () => {
+    return (
+        <>
+ <div
+      class="d-flex flex-column justify-content-center align-items-center mt-5"
+    >
+      <div>
+        <h1>Welcome</h1>
+        <p></p>
+      </div>
+    </div>
+
+        </>
+    )
+}
+
+export default CompanyProfile;
