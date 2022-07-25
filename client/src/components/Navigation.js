6c946d609cc6ee295b6beb11518860c9c659c2c0
Ali Momen <98864491+alimomen10@users.noreply.github.com>
2022-07-25 19:37:10 -0400

    Navbar optimization, social icons

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -18,77 +18,79 @@ const Navigation = ({ setActivePage, activePage }) => {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg bg-light sticky-top">
+    <nav className="navbar navbar-expand-lg sticky-top" >
       <div className="container-fluid">
         <a
-          className={`navbar-brand navItem ${activePage.Home ? 'active' : ''}`}
+          className={`navbar-brand  ${activePage.Home ? 'active' : ''}`}
           href="#Home"
           onClick={(e) => handlePage(e)}
         >
-         <img src={require('../assets/img/logos/Locum-logos.jpeg')}/>
+         <img width="10%" height="auto" src={require('../assets/img/logos/Locum-logos_transparent.png')}/>
           </a>
         <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#navbarText"
-          aria-controls="navbarText"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarText">
+        <div className="collapse navbar-collapse" id="navbarNav">
           {Auth.loggedIn() ? (
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a
-                  className={`nav-link navItem ${
-                    activePage.Dashboard ? 'active' : ''
-                  }`}
-                  aria-current="page"
-                  href="#Dashboard"
-                  onClick={(e) => handlePage(e)}
-                >
-                  Dashboard
-                </a>
-              </li>
-              <li>
-                <a href="/" onClick={logout}>
-                  Logout
-                </a>
-              </li>
-            </ul>
+            
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a
+                    className={`nav-link navItem ${
+                      activePage.Dashboard ? 'active' : ''
+                    }`}
+                    aria-current="page"
+                    href="#Dashboard"
+                    onClick={(e) => handlePage(e)}
+                  >
+                    Dashboard
+                  </a>
+                </li>
+                <li>
+                  <a href="/" onClick={logout}>
+                    Logout
+                  </a>
+                </li>
+              </ul>
+            
           ) : (
-            <ul>
-              <li>
-                <a
-                  className={`nav-link navItem ${
-                    activePage.Login ? 'active' : ''
-                  }`}
-                  aria-current="page"
-                  href="#Login"
-                  onClick={(e) => handlePage(e)}
-                >
-                  Login
-                </a>
-              </li>
-              <li>
-                <a
-                  className={`nav-link navItem ${
-                    activePage.Signup ? 'active' : ''
-                  }`}
-                  aria-current="page"
-                  href="#Signup"
-                  onClick={(e) => handlePage(e)}
-                >
-                  Signup
-                </a>
-              </li>
-            </ul>
+              <ul className='d-flex'>
+                <li className='me-2'>
+                  <a
+                    className={`nav-link navItem ${
+                      activePage.Login ? 'active' : ''
+                    }`}
+                    aria-current="page"
+                    href="#Login"
+                    onClick={(e) => handlePage(e)}
+                  >
+                    Login
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className={`nav-link navItem ${
+                      activePage.Signup ? 'active' : ''
+                    }`}
+                    aria-current="page"
+                    href="#Signup"
+                    onClick={(e) => handlePage(e)}
+                  >
+                    Signup
+                  </a>
+                </li>
+              </ul>
           )}
         </div>
-        <span className="navbar-text"> Staffing. Revolutionized.</span>
+        <div><h6 className="navbar-text text-white"> Staffing. Revolutionized.</h6></div>
       </div>
     </nav>
   );
