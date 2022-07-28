1fee3f3c9b4812f47a5362311f723b31adf31238
Csabbah <csabbah@ryerson.ca>
2022-07-28 17:55:40 -0400

    Minor update in index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,13 +6,13 @@ router.use('/api', apiRoutes);
 
 // serve up react front-end in production
 // User this when working on development project
-router.use((req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/index.html'));
-});
+// router.use((req, res) => {
+//   res.sendFile(path.join(__dirname, '../../client/index.html'));
+// });
 
 // Use this when deploying
-// router.use((req, res) => {
-//   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
-// });
+router.use((req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+});
 
 module.exports = router;
